cc36406cf76cf04929e5e226617159a8b14d737c
pavankumar264-sys <pavankumar462004@gmail.com>
2025-07-28 20:58:12 +0530

    Add Project File

---
+++ models/listing.js
@@ -0,0 +1,62 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+const review=require("./review.js");
+
+const listingSchema=new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:String,
+    image: {
+      url:String,
+      filename:String,
+    }
+,
+    price:Number,
+    location:String,
+    country:String,
+
+    reviews:[{
+      type:Schema.Types.ObjectId,
+      ref:"Review",
+    }
+    ],
+    owner:{
+      type:Schema.Types.ObjectId,
+      ref:"User"
+    },
+    geometry:{
+      type:{
+      type:String,
+      enum:["Point"],
+      required:true
+    },
+    coordinates:{
+      type:[Number],
+      required:true
+    },
+
+  },
+  // mobile:{
+  //   type:String,
+  //   minlength:10,
+  //   maxlength:10
+  // },
+
+  
+  // category:{
+  //   type:String,
+  //   enum:["mountains","arctic","farms","deserts"]
+  // }
+});
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+     await review.deleteMany({reviews:{$in:listing.reviews}})
+    }
+   
+})
+
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
