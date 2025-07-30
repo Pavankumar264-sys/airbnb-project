37e9a5a7ad81a36aedd44e5b7234ec5c388ef7c5
pavankumar264-sys <pavankumar462004@gmail.com>
2025-07-30 19:04:31 +0530

    session information

--- app.js
+++ app.js
@@ -54,7 +54,9 @@ const sessionOptions={
     cookie:{
         expires:Date.now()+7*24*60*60*1000,
         maxAge:7*24*60*60*1000,
-        httpOnly:true
+        httpOnly:true,
+        sameSite:'lax',
+        secure:process.env.NODE_ENV=="production"
         
     },
 };
